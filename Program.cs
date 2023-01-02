bd42e64845cbdf8000c4c591d9a7da0da70bc457
Anisha0906 <anishadas880@gmail.com>
2023-01-02 20:32:56 +0530

    [Anisha]Add: Line Comparision Computation

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           Console.WriteLine("Welcome to Line Comparison Computation");
+           Console.ReadKey();
+        }
+    }
+
+}
