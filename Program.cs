6efd8c142d4f98578b6e26d97d9e23379057b9e3
Anisha0906 <anishadas880@gmail.com>
2023-01-02 20:54:34 +0530

    [Anisha]Add:Calculate Length Of a Line

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LineComparision;
+using System;
 
 namespace LineComparison
 {
@@ -8,7 +9,17 @@ namespace LineComparison
         {
 
            Console.WriteLine("Welcome to Line Comparison Computation");
-           Console.ReadKey();
+            Console.WriteLine("Choose Option\n 1.Length Of A Line");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    LineComparisionComputation.CalculateLength();
+                    break;
+                default:
+                    Console.WriteLine("Entered Wrong Choice");
+                    break;
+            }
         }
     }
 
