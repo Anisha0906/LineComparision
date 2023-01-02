e408b11737586931b7629625242ad9beb1172e2e
Anisha0906 <anishadas880@gmail.com>
2023-01-02 21:17:54 +0530

    [Anisha]Add:Compare Length of Two lines using CompareTo method

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace LineComparison
         {
 
            Console.WriteLine("Welcome to Line Comparison Computation");
-            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines");
+            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines\t 3.CompareLength");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -18,6 +18,9 @@ namespace LineComparison
                     break;
                 case 2:
                     EqualityOfTwoLines.Equality();
+                    break;
+                case 3:
+                    CompareLengthOFTwoLines.CompareLength();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
