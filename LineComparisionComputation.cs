fa137acdf502346d056de6132d6ae227a8bf2277
Anisha0906 <anishadas880@gmail.com>
2023-01-10 18:06:40 +0530

    [Anisha]Refactor:Using OOPs concept and compareTo method for line comparision

--- LineComparisionComputation.cs
+++ LineComparisionComputation.cs
@@ -10,16 +10,16 @@ namespace LineComparision
     {
             public static void CalculateLength()
             {
-                int x1, x2, y1, y2;
-                Console.Write("Input an integer value: +x1 +x2 +y1 +y2 ");
-                x1 = Convert.ToInt32(Console.ReadLine());
-                x2 = Convert.ToInt32(Console.ReadLine());
-                y1 = Convert.ToInt32(Console.ReadLine());
-                y2 = Convert.ToInt32(Console.ReadLine());
-                double distance;
-                distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-                Console.WriteLine("distance =" + distance);
-            }
-        
+            int x1, x2, y1, y2;
+            Console.Write("Input an integer value: +x1 +x2 +y1 +y2 ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double distance;
+            distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("distance =" + distance);
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace LineComparison
         {
 
            Console.WriteLine("Welcome to Line Comparison Computation");
-            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines\t 3.CompareLength");
+            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines\t 3.CompareLength\t 4.Comparing lines Using OOPs concept");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -21,6 +21,15 @@ namespace LineComparison
                     break;
                 case 3:
                     CompareLengthOFTwoLines.CompareLength();
+                    break;
+                case 4:
+                    LineComparisionUsingOOPs obj = new LineComparisionUsingOOPs();
+                    obj.SetLength1(3, 4, 7, 8);
+                    obj.GetLength1();
+                    obj.SetLength2(3, 4, 5, 8);
+                    obj.GetLength2();
+                    obj.display1();
+                    obj.display2();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
